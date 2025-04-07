33a6ac3c1d5d811e17c2ef5911a4c82beb5a3cd9
Darii Kishchak <dkishchak6536@stu.d214.org>
2025-04-07 13:33:38 -0500

    Added textfield with name

--- ContentView.swift
+++ ContentView.swift
@@ -1,11 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var name = ""
     var body: some View {
         Text("Lily Makula")
        Text("Darii")
         Button("Press me") {
             
         }
+        TextField("Enter your name", text: $name)
+        
+        Text("Hi, \(name)")
     }
 }
